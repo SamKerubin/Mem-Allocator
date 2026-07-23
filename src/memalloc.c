6d0eb6eb4d995905bf18023b1a8704acf6823462
SamKerubin <samuelkiller2013@gmail.com>
2026-07-22 22:37:28 -0500

    Added prototype of malloc

--- src/memalloc.c
+++ src/memalloc.c
@@ -14,9 +14,9 @@
 #define MMAP_ALLOC_FLAG     (1 << 1)
 #define SIZE_FLAGS          OCCUPANCY_FLAG | MMAP_ALLOC_FLAG
 
-#define GET_REAL_SIZE(sz)   ((sz) & ~(SIZE_FLAGS))
+#define GET_REAL_SIZE(b)    ((b)->size & ~(SIZE_FLAGS))
 #define IS_BLOCK_FREE(b)    ((b)->size & OCCUPANCY_FLAG)
-#define IS_MMAP_ALLOC(sz)   ((sz) & MMAP_ALLOC_FLAG)
+#define IS_MMAP_ALLOC(b)    ((b)->size & MMAP_ALLOC_FLAG)
 
 #define MMAP_ALLOC_THRESHOLD 131072 // 128kb max before switching to mmap allocations
 
@@ -33,9 +33,59 @@ struct M_footer {
 
 M_header *head, *tail;
 
-void *m_alloc(size_t size) {
-    (void)size;
+static M_header *find_free_block(size_t size) {
+    M_header *curr = head;
+
+    while (curr) {
+        // First fit
+        if (IS_BLOCK_FREE(curr) && GET_REAL_SIZE(curr) >= size) {
+            // TODO: Handle block splitting
+            return curr;
+        }
+        curr = curr->next;
+    }
     return NULL;
+}
+
+void *m_alloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
+
+    void *block;
+    M_header *header;
+    size_t total_size;
+
+    if (size >= MMAP_ALLOC_THRESHOLD) {
+        // Alloc using mmap
+        return NULL;
+    }
+
+    total_size = sizeof(size_t) + ALIGN_SIZE(size) + sizeof(M_footer);
+
+    header = find_free_block(total_size);
+    if (header) {
+        header->size &= ~(SIZE_FLAGS);
+        return (void *)(header + 1);
+    }
+
+    block = sbrk(total_size);
+    if (block == MEM_FAIL) {
+        return NULL;
+    }
+
+    header = block;
+    header->size = total_size | OCCUPANCY_FLAG;
+
+    if (!head) {
+        head = header;
+    }
+    if (tail) {
+        tail->next = header;
+    }
+    tail = header;
+
+    return (void *)(header + 1);
 }
 
 void m_free(void *ptr) {
